ad853939a838fb0b85294b47b38460baa4932ad6
haszari <haszari@cartoonbeats.com>
2020-03-10 17:02:00 +1300

    tag cloud styling

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -3,19 +3,36 @@ import { useSelector } from 'react-redux';
 
 import { getTags } from '../store/selectors';
 
+const tagFontSize = function( tagHitCount, maxHitCount ) {
+  const minSize = 8, sizeRange = 30;
+  var size = 0;
+  size = tagHitCount / maxHitCount;
+  size = size * sizeRange;
+  return size + minSize;
+};
+
 function Tags() {
   const tags = useSelector( getTags );
+  if ( ! tags || ! tags.length ) {
+    return null;
+  }
 
-  return tags.map( ( tag ) => {
+  const maxHitCount = tags[0].count;
+
+  const cloud = tags.map( ( tag ) => {
     const styles = {
-      fontSize: tag.count / 10,
-      display: 'inline-block',
-      margin: '10px',
+      fontSize: tagFontSize( tag.count, maxHitCount ),
     };
     return (
       <div className='tag' style={ styles } key={ tag._id } >{ tag.tag }</div> 
     );
   } );
+
+  return (
+     <div className='tags'>
+       { cloud }
+     </div>
+  );
 }
 
 export default Tags;
