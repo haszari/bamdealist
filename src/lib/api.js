2f9817d561abc4cb8b68cd868521ab03d1710875
haszari <haszari@cartoonbeats.com>
2020-03-22 11:55:49 +1300

    use correct API path in production - fix app on mobile/other client device

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,2 +1,9 @@
-export const apiBase = 'http://localhost:8947/api/v1/';
+
+const apiPath = '/api/v1/';
+const dev = `//${ process.env.REACT_APP_EXTERNAL_HOSTNAME }:${ process.env.REACT_APP_EXTERNAL_BIND_PORT }${ apiPath }`;
+const prod = `${ apiPath }`;
+
+export const apiBase = ( process.env.NODE_ENV === 'development' ) 
+  ? dev
+  : prod;
 
