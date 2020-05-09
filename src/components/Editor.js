1111a0c2d5efa626c7dfff400eccc2ca8f7e46eb
haszari <haszari@cartoonbeats.com>
2020-05-09 15:23:03 +1200

    prototype editor using react-mde

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import ReactMde from "react-mde";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+import markdownRenderer from '../lib/markdown-renderer';
+
 
 function Editor () {
+  const [value, setValue] = React.useState("**Hello BAM!!!**");
+  const [selectedTab, setSelectedTab] = React.useState("write");
   return (
-    <p>Editor coming soon!</p>
+      <ReactMde
+        value={value}
+        onChange={setValue}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={ markdown =>
+          Promise.resolve( markdownRenderer( markdown ) )
+        }
+      />      
   );
 }
 
