ff9220b76154daba74c58a3d0fa0b790863f8d61
haszari <haszari@cartoonbeats.com>
2020-06-04 20:02:55 +1200

    new favicon (etc)

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import Button from '@material-ui/core/Button';
+
 import markdownRenderer from '../lib/markdown-renderer';
 
 import { getArticle } from '../store/article/selectors';
@@ -11,15 +13,20 @@ function Item() {
   const item = useSelector( getArticle );
 
   return (
-    <div className='article' key={ item._id } >
-      <h2 className='title' dangerouslySetInnerHTML={{
-        __html:  markdownRenderer( item.title )
-      }} />
-      <div className='content'  dangerouslySetInnerHTML={{
-        __html:  markdownRenderer( item.content )
-      }} /> 
-      <TagList tags={ item.tags } /> 
-    </div> 
+    <>
+      <div className='article' key={ item._id } >
+        <h2 className='title' dangerouslySetInnerHTML={{
+          __html:  markdownRenderer( item.title )
+        }} />
+        <div className='content'  dangerouslySetInnerHTML={{
+          __html:  markdownRenderer( item.content )
+        }} /> 
+        <TagList tags={ item.tags } /> 
+      </div> 
+      <div className='article-toolbar'>
+        <Button>Edit</Button>
+      </div>
+    </>
   );
 }
 
