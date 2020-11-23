4897f454688ec678390f9597ee9abb847f326b78
haszari <haszari@cartoonbeats.com>
2020-11-23 20:46:02 +1300

    stub file drop handler - log file name

--- src/components/DragDropImporter.js
+++ src/components/DragDropImporter.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+
+function filesDropped( event ) {
+  event.preventDefault();
+  console.log( arguments );
+
+  if ( event.dataTransfer.items ) {
+    for ( let i = 0; i < event.dataTransfer.items.length; i++ ) {
+      if (event.dataTransfer.items[i].kind === 'file') {
+        const file = event.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  }
+}
+
 function DragDropImporter() {
   return (
     <div className='article importer' >
       <h2 className='title'>Drag & drop a file to import</h2>
-      <div className='drop-target'></div>
+      <div 
+        className='drop-target'
+        onDragOver={ 
+          // Prevent the browser from intercepting and opening the file in a tab.
+          event => event.preventDefault()
+        }
+        onDrop={ filesDropped }
+      ></div>
     </div> 
   );
 }
