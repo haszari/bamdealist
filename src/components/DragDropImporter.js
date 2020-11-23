fbe981928952c256d0e129151b1cf2c372447e11
haszari <haszari@cartoonbeats.com>
2020-11-23 20:25:52 +1300

    placeholder component & routing for /import page

---
+++ src/components/DragDropImporter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DragDropImporter() {
+  return (
+    <div className='article importer' >
+      <h2 className='title'>Drag & drop a file to import</h2>
+      <div className='drop-target'></div>
+    </div> 
+  );
+}
+
+export default DragDropImporter;
